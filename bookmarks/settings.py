e1cacc53cd5ad965e7fba00a015bc983dad6a9ae
student03 <a@a.com>
2024-10-28 15:07:30 +0100

    Paseczek  huhuhu

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGOUT_URL = 'logout'
