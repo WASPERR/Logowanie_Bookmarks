63394cca5799a5fe9c4142079ca88574ae46d73e
student03 <a@a.com>
2024-11-05 15:31:19 +0100

    Rejestracja uzytkownika

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,21 @@
 from django import forms
+from django.contrib.auth import get_user_model
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+
+    password = forms.CharField(widget=forms.PasswordInput, label='Hasło')
+    password2 = forms.CharField(widget=forms.PasswordInput, label='Powtórz Hasło')
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'first_name', 'email']
+
+    def clean_password2(self):
+        if self.cleaned_data['password'] != self.cleaned_data['password2']:
+            raise forms.ValidationError("Hasła są różne")
+
+        return self.cleaned_data['password2']
